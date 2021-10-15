8f3541f2ad2fac7ea2b29b821f0413e8d9466704
wldzimerz <kvkad13@bk.ru>
2021-10-15 21:56:08 +0700

    feat: hits and winner realization

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-const Kitana = {
+const arenas = document.querySelector(".arenas");
+const button = document.querySelector(".button");
+
+const player1 = {
+  player: 1,
   name: "Kitana",
   hp: 100,
   img: "http://reactmarathon-api.herokuapp.com/assets/kitana.gif",
@@ -8,7 +12,8 @@ const Kitana = {
   },
 };
 
-const Scorpion = {
+const player2 = {
+  player: 2,
   name: "Scorpion",
   hp: 100,
   img: "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif",
@@ -18,40 +23,72 @@ const Scorpion = {
   },
 };
 
-const $arenas = document.querySelector(".arenas");
+function createNewElement(className, tag = "div") {
+  const elem = document.createElement(tag);
+  if (className) {
+    elem.classList.add(className);
+  }
 
-function createPlayer(player, obj) {
-  const $player = document.createElement("div");
-  $player.classList.add(player);
-
-  const $progressbar = document.createElement("div");
-  $progressbar.classList.add("progressbar");
-
-  $player.appendChild($progressbar);
-
-  const $life = document.createElement("div");
-  $life.classList.add("life");
-  $life.style.width = obj.hp + "%";
-
-  const $name = document.createElement("div");
-  $name.classList.add("name");
-  $name.innerText = obj.name;
-
-  $progressbar.appendChild($life);
-  $progressbar.appendChild($name);
-
-  const $character = document.createElement("div");
-  $character.classList.add("character");
-
-  $player.appendChild($character);
-
-  const $img = document.createElement("img");
-  $img.src = obj.img;
-
-  $character.appendChild($img);
-
-  return $player;
+  return elem;
 }
 
-$arenas.appendChild(createPlayer("player1", Scorpion));
-$arenas.appendChild(createPlayer("player2", Kitana));
+function createPlayer(obj) {
+  const player = createNewElement("player" + obj.player);
+
+  const progressbar = createNewElement("progressbar");
+  player.appendChild(progressbar);
+
+  const life = createNewElement("life");
+  life.style.width = obj.hp + "%";
+
+  const name = createNewElement("name");
+  name.innerText = obj.name;
+
+  progressbar.appendChild(life);
+  progressbar.appendChild(name);
+
+  const character = createNewElement("character");
+
+  player.appendChild(character);
+
+  const img = document.createElement("img");
+  img.src = obj.img;
+
+  character.appendChild(img);
+
+  return player;
+}
+
+arenas.appendChild(createPlayer(player1));
+arenas.appendChild(createPlayer(player2));
+
+function hitPlayer(player) {
+  const randomHp = Math.ceil(Math.random() * 20);
+
+  const playerLife = document.querySelector(`.player${player.player} .life`);
+  player.hp -= randomHp;
+
+  if (player.hp <= 0) {
+    playerLife.style.width = 0 + "%";
+    arenas.appendChild(playerWin(player.name));
+    button.disabled = true;
+  } else {
+    playerLife.style.width = `${player.hp}%`;
+  }
+}
+
+function playerWin(name) {
+  const winTitle = createNewElement("winTitle");
+  if (name === player2.name) {
+    winTitle.innerText = player1.name + " wins";
+  } else {
+    winTitle.innerText = player2.name + " wins";
+  }
+
+  return winTitle;
+}
+
+button.addEventListener("click", () => {
+  hitPlayer(player1);
+  hitPlayer(player2);
+});
